24c7697e19642d4f5d0e518d394abbaae70d16df
Дмитрий <batarond@gmail.com>
2019-08-26 01:53:59 +0300

    mas
    ЧитаетМассив по 5 бит

---
+++ Mas5B.c
@@ -0,0 +1,45 @@
+﻿#include <conio.h>
+#include <stdio.h>
+/*
+	1) db 0, 0, 0, 0, 0
+	2) db 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+	3) db 'aaaaa'
+	4) db 'abcde'
+	Дан массив из 5 байт. Рассматривая его как массив из восьми
+	5-битных байтов, посчитать количество байт с четным числом единиц.
+
+*/
+void main() {
+	char mas1[] = { 0,0,0,0,0 };
+	
+	int buf=0, kik=0, kol=0;
+	
+	for (int i = 0, sdvig = 0; i < 4; i++, sdvig += 8) {
+		buf |= mas1[i] << sdvig;
+	}
+	
+	for (int i = 0, k=2; i < 6; i++) {
+		int z = 0;
+		kik = 0; // количество едениц
+		while (z < 5) {
+			if ((buf & k) >0) kik++;
+			k *= 2;
+			z++;
+		}
+		if ((kik % 2) == 0) kol++; // кол количесвто байт с четными единичными разрядами
+	}
+	buf = (buf & -1073741824) >> 30;
+	buf |= mas1[4] << 2;
+	
+	for (int i = 0, k = 2; i < 2; i++) {
+		int z = 0;
+		kik = 0;
+		while (z < 5) {
+			if ((buf & k) > 0) kik++;
+			k *= 2;
+			z++;
+		}
+		if ((kik % 2) == 0) kol++;
+	}
+		//printf_s("%d", kol);;
+}
--- visibility.c
+++ visibility.c
@@ -5,10 +5,17 @@
  контролем процесса выполнения программы, преобразованием типов и другие. 
  Заголовок вполне совместим с C++ и известен в нём как cstdlib.
  Название «stdlib» расшифровывается как «standard library» (стандартная библиотека). */
+#include <stdint.h>
 
 void main()
-{	
-	system("chcp 1251");
-	printf("Димончик Не выводит\n");
-	_getch();
+{
+	struct address {
+		char* name; // имя "Jim Dandy" 
+		long  number; // номер дома 61  
+		char* street; // улица "South Street" 
+		char* town; // город "New Providence"  
+		char* state[2]; // штат 'N' 'J' 
+		int   zip; // индекс 7974 
+	};
+	struct address jd = { "Jim Dandy",  61, "South Street",  "New Providence", {'N','J'}, 7974 };;
 }
